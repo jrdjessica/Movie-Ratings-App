d42a90ac160f6e4e3b9cbb0b034709f77e30117e
Jessica Dea <jessicardea@gmail.com>
2022-09-08 12:25:46 -0700

    Create rating function

--- crud.py
+++ crud.py
@@ -20,6 +20,14 @@ def create_movie(title, overview, release_date, poster_path):
     return movie
 
 
+def create_rating(user, movie, score):
+    """Create and return a new rating."""
+
+    rating = Rating(user=user, movie=movie, score=score)
+
+    return rating
+
+
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
