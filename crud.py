7e1069b73dd434c1754c797ea00b6ecc31e96155
Jessica Dea <jessicardea@gmail.com>
2022-09-08 12:17:26 -0700

    Create movie class

--- crud.py
+++ crud.py
@@ -11,6 +11,15 @@ def create_user(email, password):
     return user
 
 
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+
+    movie = Movie(title=title, overview=overview,
+                  release_date=release_date, poster_path=poster_path)
+
+    return movie
+
+
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
